0273e9f990e2125fa1976e65ba2cbbdbe75610c0
pushpendra.khandelwal@kuliza.com <pushpendra.khandelwal@kuliza.com>
2018-06-12 23:34:51 +0530

    logger middleware from redux-logger

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import Reducers from './Reducer/Reducers';
+import {createLogger} from 'redux-logger'
 
-const store = createStore(Reducers) // Normally it is an object {}
+const middleware = applyMiddleware(createLogger())
+
+const store = createStore(Reducers, middleware) // Normally it is an object {}
 
 export default store
