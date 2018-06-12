de10de94123cd3a2f4cea18c6209818148af4780
pushpendra.khandelwal@kuliza.com <pushpendra.khandelwal@kuliza.com>
2018-06-12 23:40:11 +0530

    why to use thunk - for async action

--- src/App.js
+++ src/App.js
@@ -17,12 +17,24 @@ store.subscribe(() => {
   console.log("Store changed", store.getState())
 })
 
-// Fire Action
+// Fire Action as Plain Objects
 store.dispatch({type: 'CHANGE_AGE', payload: 32})
 store.dispatch({type: 'CHANGE_NAME', payload: 'Raju'})
 store.dispatch({type: 'CHANGE_EMP', payload: {age: 23, name: 'Ram'}})
 store.dispatch({type: 'ADD_TASK', payload: 'CRS'})
 store.dispatch({type: 'ADD_TASK', payload: 'CeS'})
 store.dispatch({type: 'DELETE_TASK', payload: 'CRS'})
+
+
+// If you do like this, this is not allowed
+// Error: Actions must be plain objects. Use custom middleware for async actions.
+
+/*store.dispatch((dispatch) => {
+  store.dispatch({type: 'ADD_TASK', payload: 'Turvo'})
+  store.dispatch({type: 'ADD_TASK', payload: 'Acko'})
+  store.dispatch({type: 'ADD_TASK', payload: 'HDFC'})
+}) */
+
+// to solve this we can use thunk middle ware which is provided by 'react-thunk'
 
 export default App;
